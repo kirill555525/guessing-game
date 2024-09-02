9ac7c54658811628a575681f5b2daf4d6567570e
kirill555525 <surskillwill@gmail.com>
2024-09-02 23:50:48 +0500

    feat: complete task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,25 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.range = [];
+    }
 
     setRange(min, max) {
+        for (let i = min + 1; i < max; i++) {
+            this.range.push(i);
+        }
 
     }
 
     guess() {
-
+        return this.range[Math.floor(this.range.length / 2)];
     }
 
     lower() {
-
+        this.range = this.range.slice(0, Math.floor(this.range.length / 2));
     }
 
     greater() {
-
+        this.range = this.range.slice(Math.floor(this.range.length / 2) + 1);
     }
 }
 
